c62764fbb0e583260fad4e684e18b3de2c67a5dc
Evandro Felipe Rodrigues Lima De Carvalho <30915993+evandrorodrigues@users.noreply.github.com>
2025-02-26 12:37:19 -0300

    Create EscalaMedicos.js

---
+++ frontend/src/EscalaMedicos.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function EscalaMedicos() {
+  return (
+    <div>
+      <h2>Gerenciamento de Escala Médica</h2>
+      <p>Este é o componente onde será feita a escala dos médicos.</p>
+    </div>
+  );
+}
+
+export default EscalaMedicos;
